feb424149214358c287d92bd7565a4b40f1438ef
Piotr Bogdan <ppbogdan@gmail.com>
2020-08-20 16:12:19 +0100

    gnome3.gdm: reformat / tidy up the expression

--- pkgs/desktops/gnome-3/core/gdm/default.nix
+++ pkgs/desktops/gnome-3/core/gdm/default.nix
@@ -1,10 +1,30 @@
-{ stdenv, fetchurl, substituteAll, pkgconfig, glib, itstool, libxml2, xorg
-, accountsservice, libX11, gnome3, systemd, autoreconfHook, dconf
-, gtk3, libcanberra-gtk3, pam, libtool, gobject-introspection, plymouth
-, librsvg, coreutils, xwayland, nixos-icons, fetchpatch }:
+{ stdenv
+, autoreconfHook
+, dconf
+, fetchurl
+, itstool
+, libtool
+, libxml2
+, pkg-config
+, substituteAll
 
+, accountsservice
+, coreutils
+, glib
+, gnome3
+, gobject-introspection
+, gtk3
+, libX11
+, libcanberra-gtk3
+, librsvg
+, nixos-icons
+, pam
+, plymouth
+, systemd
+, xorg
+, xwayland
+}:
 let
-
   icon = fetchurl {
     url = "https://raw.githubusercontent.com/NixOS/nixos-artwork/4f041870efa1a6f0799ef4b32bb7be2cafee7a74/logo/nixos.svg";
     sha256 = "0b0dj408c1wxmzy6k0pjwc4bzwq286f1334s3cqqwdwjshxskshk";
@@ -14,9 +34,7 @@ let
     src = ./org.gnome.login-screen.gschema.override;
     inherit icon;
   };
-
 in
-
 stdenv.mkDerivation rec {
   pname = "gdm";
   version = "3.34.1";
@@ -25,32 +43,6 @@ stdenv.mkDerivation rec {
     url = "mirror://gnome/sources/gdm/${stdenv.lib.versions.majorMinor version}/${pname}-${version}.tar.xz";
     sha256 = "1lyqvcwxhwxklbxn4xjswjzr6fhjix6h28mi9ypn34wdm9bzcpg8";
   };
-
-  # Only needed to make it build
-  preConfigure = ''
-    substituteInPlace ./configure --replace "/usr/bin/X" "${xorg.xorgserver.out}/bin/X"
-  '';
-
-  initialVT = "7";
-
-  configureFlags = [
-    "--sysconfdir=/etc"
-    "--localstatedir=/var"
-    "--with-plymouth=yes"
-    "--enable-gdm-xsession"
-    "--with-initial-vt=${initialVT}"
-    "--with-systemdsystemunitdir=$(out)/etc/systemd/system"
-    "--with-udevdir=$(out)/lib/udev"
-  ];
-
-  nativeBuildInputs = [ pkgconfig libxml2 itstool autoreconfHook libtool dconf ];
-  buildInputs = [
-    glib accountsservice systemd
-    gobject-introspection libX11 gtk3
-    libcanberra-gtk3 pam plymouth librsvg
-  ];
-
-  enableParallelBuilding = true;
 
   patches = [
     # Change hardcoded paths to nix store paths.
@@ -76,16 +68,56 @@ stdenv.mkDerivation rec {
     ./reset-environment.patch
   ];
 
-  installFlags = [
-    "sysconfdir=$(out)/etc"
-    "dbusconfdir=$(out)/etc/dbus-1/system.d"
+  nativeBuildInputs = [
+    autoreconfHook
+    dconf
+    itstool
+    libtool
+    libxml2
+    pkg-config
   ];
+
+  buildInputs = [
+    accountsservice
+    glib
+    gobject-introspection
+    gtk3
+    libX11
+    libcanberra-gtk3
+    librsvg
+    pam
+    plymouth
+    systemd
+  ];
+
+  # Only needed to make it build
+  preConfigure = ''
+    substituteInPlace ./configure --replace "/usr/bin/X" "${xorg.xorgserver.out}/bin/X"
+  '';
+
+  initialVT = "7";
+
+  configureFlags = [
+    "--sysconfdir=/etc"
+    "--localstatedir=/var"
+    "--with-plymouth=yes"
+    "--enable-gdm-xsession"
+    "--with-initial-vt=${initialVT}"
+    "--with-systemdsystemunitdir=$(out)/etc/systemd/system"
+    "--with-udevdir=$(out)/lib/udev"
+  ];
+
+  enableParallelBuilding = true;
 
   preInstall = ''
     schema_dir=${glib.makeSchemaPath "$out" "${pname}-${version}"}
     install -D ${override} $schema_dir/org.gnome.login-screen.gschema.override
   '';
 
+  installFlags = [
+    "sysconfdir=$(out)/etc"
+    "dbusconfdir=$(out)/etc/dbus-1/system.d"
+  ];
   passthru = {
     updateScript = gnome3.updateScript {
       packageName = "gdm";

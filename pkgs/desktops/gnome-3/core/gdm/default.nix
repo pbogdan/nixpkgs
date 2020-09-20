dd4b7e46d1b8b464aff10f26b43405d8d65b66f3
Piotr Bogdan <ppbogdan@gmail.com>
2020-09-20 14:40:32 +0100

    Revert "gnome3.gdm: revert "session: Always use separate session bus for greeter sessions""
    This reverts commit 0ef80a08fb4b78039ff14764bac33ebb8afcf3c6.

--- pkgs/desktops/gnome-3/core/gdm/default.nix
+++ pkgs/desktops/gnome-3/core/gdm/default.nix
@@ -1,7 +1,6 @@
 { stdenv
 , dconf
 , fetchurl
-, fetchpatch
 , itstool
 , libtool
 , libxml2
@@ -48,14 +47,6 @@ stdenv.mkDerivation rec {
   };
 
   patches = [
-    (fetchpatch {
-      url = "https://gitlab.gnome.org/GNOME/gdm/-/commit/febeb9a9295fcd47a63c8ccdb41b88387acf8aac.diff";
-      sha256 = "0fc13li3hv7dnhmb8mn20svicbm2z3q1vnbjxnjzw7v80ga2qljg";
-      revert = true;
-      excludes = [
-        "NEWS"
-      ];
-    })
     # Change hardcoded paths to nix store paths.
     (substituteAll {
       src = ./fix-paths.patch;
